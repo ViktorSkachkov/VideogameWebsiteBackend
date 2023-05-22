60df9f1310a1b3f1d604e95c9f8f4814057a5837
i459452 <459452@student.fontys.nl>
2023-05-22 15:30:39 +0200

    Tests

--- backend/src/test/java/com/example/demo/business/impl/gameorder/GetGameOrdersRankedUseCaseImplTest.java
+++ backend/src/test/java/com/example/demo/business/impl/gameorder/GetGameOrdersRankedUseCaseImplTest.java
@@ -29,7 +29,7 @@ class GetGameOrdersRankedUseCaseImplTest {
     private GetGameOrdersRankedUseCaseImpl getGameOrdersRankedUseCase;
 
     @Test
-    void getGameOrdersRanked() {
+    void getGameOrdersRankedWith0() {
         LocalDateTime time = LocalDateTime.now();
 
         GameOrder gameOrder1 = GameOrder.builder()
@@ -60,6 +60,142 @@ class GetGameOrdersRankedUseCaseImplTest {
         when(gameOrderRepository.findAll())
                 .thenReturn(List.of(gameOrderPersistence1));
         List<RankingGameOrder> actualResult = getGameOrdersRankedUseCase.getGameOrdersRanked(0);
+
+        List<RankingGameOrder> expectedResult = new ArrayList<>();
+        expectedResult.add(rankingGameOrder);
+
+        assertEquals(expectedResult, actualResult);
+        verify(gameOrderRepository).findAll();
+    }
+
+    @Test
+    void getGameOrdersRankedWith1() {
+        LocalDateTime time = LocalDateTime.now();
+
+        GameOrder gameOrder1 = GameOrder.builder()
+                .id(0)
+                .game(41)
+                .user(41)
+                .units(2)
+                .totalPrice(0)
+                .time(time)
+                .build();
+        GameOrder gameOrder2 = GameOrder.builder()
+                .id(1)
+                .game(41)
+                .user(41)
+                .units(2)
+                .totalPrice(0)
+                .time(time)
+                .build();
+        GameOrderPersistence gameOrderPersistence1 = GameOrderPersistence.builder()
+                .id(0)
+                .game(41)
+                .user(41)
+                .units(2)
+                .approved(true)
+                .time(time)
+                .build();
+        GameOrderPersistence gameOrderPersistence2 = GameOrderPersistence.builder()
+                .id(1)
+                .game(41)
+                .user(41)
+                .units(2)
+                .approved(true)
+                .time(time)
+                .build();
+        RankingGameOrder rankingGameOrder = RankingGameOrder.builder()
+                .id(0L)
+                .price(0)
+                .gameOrderList(List.of(gameOrder1, gameOrder2))
+                .totalPrice(0)
+                .reviewedItemId(41)
+                .numberOfTimesBought(4)
+                .build();
+
+        when(gameOrderRepository.findAll())
+                .thenReturn(List.of(gameOrderPersistence1, gameOrderPersistence2));
+        List<RankingGameOrder> actualResult = getGameOrdersRankedUseCase.getGameOrdersRanked(1);
+
+        List<RankingGameOrder> expectedResult = new ArrayList<>();
+        expectedResult.add(rankingGameOrder);
+
+        assertEquals(expectedResult, actualResult);
+        verify(gameOrderRepository).findAll();
+    }
+
+    @Test
+    void getGameOrdersRankedWith6() {
+        LocalDateTime time = LocalDateTime.now();
+
+        GameOrder gameOrder1 = GameOrder.builder()
+                .id(0)
+                .game(41)
+                .user(41)
+                .units(2)
+                .totalPrice(0)
+                .time(time)
+                .build();
+        GameOrderPersistence gameOrderPersistence1 = GameOrderPersistence.builder()
+                .id(0)
+                .game(41)
+                .user(41)
+                .units(2)
+                .approved(true)
+                .time(time)
+                .build();
+        RankingGameOrder rankingGameOrder = RankingGameOrder.builder()
+                .id(0L)
+                .price(0)
+                .gameOrderList(List.of(gameOrder1))
+                .totalPrice(0)
+                .reviewedItemId(41)
+                .numberOfTimesBought(2)
+                .build();
+
+        when(gameOrderRepository.findAll())
+                .thenReturn(List.of(gameOrderPersistence1));
+        List<RankingGameOrder> actualResult = getGameOrdersRankedUseCase.getGameOrdersRanked(6);
+
+        List<RankingGameOrder> expectedResult = new ArrayList<>();
+        expectedResult.add(rankingGameOrder);
+
+        assertEquals(expectedResult, actualResult);
+        verify(gameOrderRepository).findAll();
+    }
+
+    @Test
+    void getGameOrdersRankedWith12() {
+        LocalDateTime time = LocalDateTime.now();
+
+        GameOrder gameOrder1 = GameOrder.builder()
+                .id(0)
+                .game(41)
+                .user(41)
+                .units(2)
+                .totalPrice(0)
+                .time(time)
+                .build();
+        GameOrderPersistence gameOrderPersistence1 = GameOrderPersistence.builder()
+                .id(0)
+                .game(41)
+                .user(41)
+                .units(2)
+                .approved(true)
+                .time(time)
+                .build();
+        RankingGameOrder rankingGameOrder = RankingGameOrder.builder()
+                .id(0L)
+                .price(0)
+                .gameOrderList(List.of(gameOrder1))
+                .totalPrice(0)
+                .reviewedItemId(41)
+                .numberOfTimesBought(2)
+                .build();
+
+        when(gameOrderRepository.findAll())
+                .thenReturn(List.of(gameOrderPersistence1));
+        List<RankingGameOrder> actualResult = getGameOrdersRankedUseCase.getGameOrdersRanked(12);
 
         List<RankingGameOrder> expectedResult = new ArrayList<>();
         expectedResult.add(rankingGameOrder);
