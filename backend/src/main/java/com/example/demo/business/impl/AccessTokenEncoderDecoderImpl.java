4a96ced865e1300a7aeb33853e60223ea247c4f1
i459452 <459452@student.fontys.nl>
2023-05-24 11:56:34 +0200

    Expiration date

--- backend/src/main/java/com/example/demo/business/impl/AccessTokenEncoderDecoderImpl.java
+++ backend/src/main/java/com/example/demo/business/impl/AccessTokenEncoderDecoderImpl.java
@@ -48,7 +48,7 @@ public class AccessTokenEncoderDecoderImpl implements AccessTokenEncoder, Access
         return Jwts.builder()
                 .setSubject(accessToken.getSubject())
                 .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(now.plus(30, ChronoUnit.MINUTES)))
+                .setExpiration(Date.from(now.plus(120, ChronoUnit.MINUTES)))
                 .addClaims(claimsMap)
                 .signWith(key)
                 .compact();
