0648ef1a02282ff3705958cba0161ae5adff74e4
i459452 <459452@student.fontys.nl>
2023-06-09 22:07:54 +0200

    Backend

--- backend/src/main/java/com/example/demo/business/cases/additionorder/CheckAdditionOrderUseCase.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.business.cases.additionorder;
-
-public interface CheckAdditionOrderUseCase {
-    boolean CheckAdditionOrder(int id);
-}
--- backend/src/main/java/com/example/demo/business/cases/gameorder/CheckGameOrderUseCase.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.business.cases.gameorder;
-
-public interface CheckGameOrderUseCase {
-    boolean CheckGameOrder(int id);
-}
